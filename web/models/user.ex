85eaf45157e3b8df88b14e6e044d0e48b80dda7c
Puspa Khanal <31549620+puspakhanal70726@users.noreply.github.com>
2018-04-11 13:20:56 -0400

    My project using phoenix framework

---
+++ web/models/user.ex
@@ -0,0 +1,23 @@
+defmodule Rumbl.User do
+  use Rumbl.Web, :model
+    schema "users" do
+      field :name, :string
+      field :username, :string
+      field :password, :string, virtual: true
+      field :password_hash, :string
+
+      timestamps()
+  end
+
+  def changeset(model, params \\ :empty) do
+    model
+      |> cast(params, ~w(name username), [])
+      |> validate_length(:username, min: 1, max: 20)
+  end
+  def registration_changeset(model, params) do model
+|> changeset(params)
+|> cast(params, ~w(password), [])
+|> validate_length(:password, min: 6, max: 100) |> put_pass_hash()
+end
+
+end
